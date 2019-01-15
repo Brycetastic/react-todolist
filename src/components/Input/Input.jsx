fcf5c90a1ddc9405860106c239f222545a591fb6
brycetastic <marbryce@gmail.com>
2019-01-14 21:07:50 -0700

    remove text value

--- src/components/Input/Input.jsx
+++ src/components/Input/Input.jsx
@@ -1,16 +1,21 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
-export default function Input({ onSubmit }) {
-    return (
-        <form onSubmit={(e) => {
-            e.preventDefault()
-            onSubmit(e.target.addtask.value);
-        }} >
-            <input type="text" name="addtask" placeholder="Add your task here" />
-            <input type="submit" value="add" />
-        </form>
-    );
+export default class Input extends Component {
+    submit = (e) => {
+        e.preventDefault();
+        this.props.onSubmit(e.target.addtask.value);
+        e.target.addtask.value = '';
+    }
+
+    render() {
+        return (
+            <form onSubmit={this.submit} >
+                <input type="text" name="addtask" placeholder="Add your task here" />
+                <input type="submit" value="add" />
+            </form>
+        );
+    }
 }
 
 Input.propTypes = {
