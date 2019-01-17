074dc3836b91002da1a75e5b428648326c1023d0
brycetastic <marbryce@gmail.com>
2019-01-17 11:36:40 -0700

    clean up

--- src/components/Switch/Switch.jsx
+++ src/components/Switch/Switch.jsx
@@ -1,16 +1,15 @@
 import React, { Component } from 'react'
 import styles from './Switch.module.css';
+import classnames from 'classnames';
 
 export default class Switch extends Component {
   render() {
     const {on, className = '', ...props} = this.props
-    const btnClassName = [
-      className,
-      styles['toggle-btn'],
-      on ? styles['toggle-btn-on'] : styles['toggle-btn-off'],
-    ]
-      .filter(Boolean)
-      .join(' ')
+    const btnClassName = classnames(className, styles['toggle-btn'], {
+      [styles['toggle-btn-on']]: on,
+      [styles['toggle-btn-off']]: !on,
+    })
+
     return (
       <div>
         <input
